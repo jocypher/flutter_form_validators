def677f0a7ae30277c89397b50ff349bef289684
willyjo1 <arthurwilchield@gmail.com>
2025-08-31 08:30:04 +0000

    fixed: resolved flutter analyze issue

--- lib/src/Validators.dart
+++ lib/src/Validators.dart
@@ -1,4 +1,5 @@
 /// A function that represents a form field validator.
 /// It takes in a string value and returns a string error message if validation fails,
 /// or null if validation passes.
+/// For example:
 typedef Validator = String? Function(String? value);
